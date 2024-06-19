de357f32c64bd070001b45cc0cd2135826f86a1c
Vadish Chhatwal <139453105+Duke-X@users.noreply.github.com>
2024-06-19 18:47:18 +0530

    Installing & Running Apache
    Created a cookbook, added a recipe in it, and recipe is always written in ruby language.

---
+++ Chef/chef_basic_1.rb
@@ -0,0 +1,34 @@
+#Before Writing the code let's create a directory cookbooks 
+# -> mkdir cookbooks
+
+#Now create a cookbook in it named as 'test-cb'
+# -> chef generate cookbook test-cb
+
+#Now move inside the cookbook you created and make a recipe named as 'test-rc'
+# -> chef generate recipe test-rc
+
+#Now open VI code editor and start writing the task you want chef to perform 
+
+#TO DOWNLOAD APACHE PACKAGE, START IT & SHOW A MESSAGE ON IT'S IP ADDRESS  
+package 'httpd' do
+  action :install
+end
+
+file '/var/www/html/index.html' do
+  content 'Apache server installed'
+  action :create
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
+
+
+  
+  
+  
+
+
+
+
+
